9b59005b7eeace86ddfc25394f0054d4e13179ec
edwinambrose87-design <edwinambrose87@gmail.com>
2026-02-16 20:10:04 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  // This allows the app to work offline-ish and satisfies the install prompt
+  event.respondWith(fetch(event.request));
+});
